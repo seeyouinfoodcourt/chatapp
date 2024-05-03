6f6707f8194279339423a95484e267e37a614e55
Mikkel Søder <mis@pentia.dk>
2024-05-03 14:05:16 +0200

    renamed app

--- android/app/src/main/java/dk/swag/awesomechatapp/MainActivity.kt
+++ android/app/src/main/java/dk/swag/awesomechatapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.awesomechatapp
+package dk.swag.awesomechatapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/dk/swag/awesomechatapp/MainApplication.kt
+++ android/app/src/main/java/dk/swag/awesomechatapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.awesomechatapp
+package dk.swag.awesomechatapp
 
 import android.app.Application
 import com.facebook.react.PackageList
