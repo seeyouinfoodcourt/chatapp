a7549571c7df1ca30b926cfd1d29ac3f2a840ec5
Mikkel Søder <mis@pentia.dk>
2024-05-06 09:36:54 +0200

    added signout button to homescreen

--- app/screens/home/home.screen.tsx
+++ app/screens/home/home.screen.tsx
@@ -2,13 +2,21 @@ import { View, Text } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Button } from '../../components/button';
 
 export const HomeScreen = () => {
     const user = auth().currentUser;
+
+    const signOut = () => {
+        auth()
+            .signOut()
+            .then(() => console.log('User signed out!'));
+    };
     return (
         <SafeAreaView>
             <Text>HomeScreen</Text>
             <Text>Email: {user?.email}</Text>
+            <Button title="Sign out" onPress={signOut} />
         </SafeAreaView>
     );
 };
