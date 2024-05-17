881001d19baf7a55cd6d013b22a0ef6465027b51
Mikkel Søder <mis@pentia.dk>
2024-05-17 10:29:37 +0200

    added image pick function to component

--- app/components/chat.image.picker.tsx
+++ app/components/chat.image.picker.tsx
@@ -1,13 +1,23 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import React, { useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import { launchImageLibrary } from 'react-native-image-picker';
 
 export const ChatImagePicker = () => {
+    const [image, setImage] = useState();
+
+    const pickImage = async () => {
+        try {
+            const result = await launchImageLibrary({ mediaType: 'photo' });
+            setImage(result);
+        } catch (error) {
+            console.log(error);
+        }
+    };
     return (
-        <View style={styles.container}>
+        <TouchableOpacity style={styles.container} onPress={() => pickImage()}>
             <Icon name="image" size={24} style={styles.icon} />
-        </View>
+        </TouchableOpacity>
     );
 };
 
