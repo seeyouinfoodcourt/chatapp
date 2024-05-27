2ab1bc959e373a02fbadbb0c74dfd8e1891063c1
Mikkel Søder <mis@pentia.dk>
2024-05-27 13:01:35 +0200

    added facebook signin for android

--- app/screens/signin/signin.screen.tsx
+++ app/screens/signin/signin.screen.tsx
@@ -6,8 +6,42 @@ import { Title } from '../../components/title';
 import { Button } from '../../components/button';
 import { GoogleSignInButton } from '../../components/google.signin';
 import { StyleSheet, Text, View } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
 
 export const SigninScreen = () => {
+    async function onFacebookButtonPress() {
+        // Attempt login with permissions
+        const result = await LoginManager.logInWithPermissions([
+            'public_profile',
+            'email',
+        ]);
+
+        console.log('result', result);
+
+        if (result.isCancelled) {
+            throw 'User cancelled the login process';
+        }
+
+        // Once signed in, get the users AccessToken
+        const data = await AccessToken.getCurrentAccessToken();
+
+        console.log('accesstoken', data);
+
+        if (!data) {
+            throw 'Something went wrong obtaining access token';
+        }
+
+        // Create a Firebase credential with the AccessToken
+        const facebookCredential = auth.FacebookAuthProvider.credential(
+            data.accessToken,
+        );
+
+        console.log('facebookcred', facebookCredential);
+
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(facebookCredential);
+    }
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.titleContainer}>
@@ -16,6 +50,10 @@ export const SigninScreen = () => {
                     Please log in to access the chat rooms
                 </Text>
             </View>
+            <Button
+                title="Facebook lort"
+                onPress={() => onFacebookButtonPress()}
+            />
 
             <GoogleSignInButton />
         </SafeAreaView>
